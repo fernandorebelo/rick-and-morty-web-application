87ab432fb1de5d12073d5bfd50f87e369f8190ec
Fernando Rebelo <fernandorebelo.dev@gmail.com>
2023-07-13 13:21:19 -0300

    refactored the code base

--- app.py
+++ app.py
@@ -4,38 +4,41 @@ import psycopg2
 
 app = Flask(__name__)
 
-# Database connection information
-host = 'localhost'
-database = 'RickAndMorty'
-user = 'postgres'
-password = 'root'
 
-try:
-    connection = psycopg2.connect(
-        host=host,
-        database=database,
-        user=user,
-        password=password
-    )
-except (Exception, psycopg2.Error) as error:
-    print("Erro ao conectar ao PostgreSQL:", error)
+def search_character(text):
+    # Database connection
+    try:
+        connection = psycopg2.connect(
+            host='localhost',
+            database='RickAndMorty',
+            user='postgres',
+            password='root'
+        )
+    except (Exception, psycopg2.Error) as error:
+        print("Erro ao conectar ao PostgreSQL:", error)
+    # Cria um cursos
+    cursor = connection.cursor()
+    # Cria sql para pesquisar no banco de dados
+    query_name = "SELECT * FROM character WHERE name ILIKE %s OFFSET 0 LIMIT 20"
+    cursor.execute(query_name, (f'%{text}%',))
+    data = cursor.fetchall()
+    # Fecha cursor
+    connection.close()
+    return data
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        text = request.form.get('text')
+        character_data = search_character(text)
+        return render_template('character.html', characters=character_data)
     return render_template('home.html')
 
 
-@app.route('/character', methods=['POST'])
+@app.route('/character')
 def character():
-    cursor = connection.cursor()
-    name_submit = request.form.get('name')
-    query_name = "SELECT * FROM character WHERE name ILIKE %s OFFSET 0 LIMIT 20"
-    cursor.execute(query_name, ('%'+name_submit+'%',))
-    rows = cursor.fetchall()
-    cursor.close()
-
-    return render_template('character.html', rows=rows)
+    return render_template('character.html', characters=character)
 
 
 if __name__ == '__main__':
