0fa42ad053515da11f101eecc1dda2b8eb8dcb1a
Fernando Rebelo <fernandorebelo.dev@gmail.com>
2023-07-13 14:06:42 -0300

    refactored code base

--- app.py
+++ app.py
@@ -1,45 +1,63 @@
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, render_template, request
+
 import psycopg2
 
 app = Flask(__name__)
 
+# Database connection configuration
+db_config = {
+    'host': 'localhost',
+    'database': 'RickAndMorty',
+    'user': 'postgres',
+    'password': 'root'
+}
 
-def search_character(text):
-    # Database connection
-    try:
-        connection = psycopg2.connect(
-            host='localhost',
-            database='RickAndMorty',
-            user='postgres',
-            password='root'
-        )
-    except (Exception, psycopg2.Error) as error:
-        print("Erro ao conectar ao PostgreSQL:", error)
-    # Cria um cursos
-    cursor = connection.cursor()
-    # Cria sql para pesquisar no banco de dados
-    query_name = "SELECT * FROM character WHERE name ILIKE %s OFFSET 0 LIMIT 20"
-    cursor.execute(query_name, (f'%{text}%',))
-    data = cursor.fetchall()
-    # Fecha cursor
-    connection.close()
-    return data
+# Function to retrieve characters from the database based on search text and pagination
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        text = request.form.get('text')
-        character_data = search_character(text)
-        return render_template('character.html', characters=character_data)
-    return render_template('home.html')
+def get_characters(search_text, offset, limit):
+    conn = psycopg2.connect(**db_config)
+    cur = conn.cursor()
+
+    # Perform the search query
+    cur.execute("SELECT * FROM character WHERE name ILIKE %s LIMIT %s OFFSET %s",
+                ('%' + search_text + '%', limit, offset))
+
+    characters = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return characters
+
+# Route to the index page
 
 
-@app.route('/character')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route to the character page
+
+
+@app.route('/character', methods=['GET', 'POST'])
 def character():
-    return render_template('character.html', characters=character)
+    if request.method == 'POST':
+        search_text = request.form['search_text']
+        offset = 0  # Initial offset for pagination
+    else:
+        search_text = request.args.get('search_text')
+        offset = int(request.args.get('offset', 0))
+
+    limit = 20  # Number of characters to retrieve
+
+    characters = get_characters(search_text, offset, limit)
+
+    prev_offset = max(offset - limit, 0)
+    next_offset = offset + limit
+
+    return render_template('character.html', characters=characters, prev_url=f'/character?search_text={search_text}&offset={prev_offset}', next_url=f'/character?search_text={search_text}&offset={next_offset}')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
